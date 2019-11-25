060fe1057be80054571677cf22a60ac2c24554e2
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-11-24 18:18:37 -0800

    add post and find friend.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var data = require("./app/data/friends");
+var friends = data.friends;
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -17,8 +18,32 @@ app.get("/survey", function(req, res){
 });
 
 app.get("/api/friends", function(req, res){
-    res.json(data.friends);
+    res.json(friends);
 })
+
+app.post("/api/friends", function(req, res){
+    const person = req.body;
+    console.log(person);
+    var matching = findFriend(person);
+    friends.push(person);
+    res.json(matching);
+});
+
+var findFriend = function(person){
+    let minDifference= 500;
+    let matchedFriend = {};
+    for(var i = 0; i < friends.length; i++){
+        let difference = 0;
+        for (var j = 0; j < friends[i].scores.length; j++){
+            difference += Math.abs(friends[i].scores[j] - person.scores[j]);
+        }
+        if (difference < minDifference){
+            minDifference = difference;
+            matchedFriend = friends[i];
+        }
+    }
+    return matchedFriend;
+}
 
 app.listen(PORT, function(){
     console.log("App listening on Port " + PORT);
