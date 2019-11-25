6b046a44f54523c66a404d41a5a1f7ccbe77161a
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-11-24 21:02:03 -0800

    add modal dialogue.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,34 @@
 var friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
+        "name": "Da Vinci",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMTlQ8RJtGl1t0KXppKcbILIm00mcjnCFjBim7BFdT4v0Pi5oO&s",
+        "scores":[
             5,
-            1,
-            4,
-            4,
             5,
+            5,
+            3,
+            2,
+            4,
             1,
             2,
-            5,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Mona Lisa",
+        "photo": "https://www.liveabout.com/thmb/9AvJcIU-fde-Hvl7iH2HAGFDVj4=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Mona_Lisa-copy-56a6e6d95f9b58b7d0e56987.jpg",
+        "scores":[
+            3,
+            3,
+            2,
             4,
-            1
+            1,
+            5,
+            2,
+            1,
+            5,
+            2
         ]
     }
 ];
